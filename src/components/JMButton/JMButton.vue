98d660828521833809cdf88e2587a35ee7207095
Julia Miocene <julia.miocene@gmail.com>
2026-09-05 23:10:40 +0200

    feat(button): add sizes and explicit disabled state

--- src/components/JMButton/JMButton.vue
+++ src/components/JMButton/JMButton.vue
@@ -2,25 +2,29 @@
 import JMIcon from "../JMIcon/JMIcon.vue";
 import "./jm-button.css";
 
-const BUTTON_VIEWS = ["primary", "secondary", "ghost"];
-
 export default {
   name: "JMButton",
   components: { JMIcon },
   props: {
+    disabled: { type: Boolean, default: false },
     iconName: { type: String, default: "" },
     text: { type: String, default: "" },
+    size: {
+      type: String,
+      default: "m",
+      validator: (value) => ["s", "m"].includes(value),
+    },
     view: {
       type: String,
       default: "primary",
-      validator: (value) => BUTTON_VIEWS.includes(value),
+      validator: (value) => ["primary", "secondary", "ghost"].includes(value),
     },
   },
 };
 </script>
 
 <template>
-  <button class="jm-button" :class="`jm-button--${view}`" type="button">
+  <button class="jm-button" :class="[`jm-button--${view}`, `jm-button--${size}`]" type="button" :disabled="disabled">
     <JMIcon v-if="iconName" :name="iconName" />
     {{ text }}
   </button>
